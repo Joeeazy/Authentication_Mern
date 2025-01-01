21a9969714523621c251001a6c2279d63690edd9
Joeeazy <joeeazy34@gmail.com>
2025-01-01 10:22:39 +0300

    handled cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js"
@@ -11,6 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({origin: "http://localhost:5173", credentials: true}))
 app.use(express.json()); //allows us to parse incoming requests from req.body
 app.use(cookieParser()) //alows us to parse incoming cookies
 
